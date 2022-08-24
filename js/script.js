24bb93e15234a50624d01f9e748d011730807b43
zyperr <ramerica200@gmail.com>
2022-08-24 04:50:59 -0300

    Arreglando errores y mejoras a la interfaz

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
 */ 
 
+//funcion para recorrer la matriz o array
 function encriptar(stringEncriptado){
     let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
     stringEncriptado = stringEncriptado.toLowerCase();
@@ -20,41 +21,50 @@ function encriptar(stringEncriptado){
     }
     return  stringEncriptado;
 }
+
+
+//Funcion  boton para encriptar
 function btnEncriptar(){
     const textoEncriptado = encriptar(inputTexto.value)
     mensaje.value = textoEncriptado;
     mensaje.style.backgroundImage = "none";
     inputTexto.value = "";
-    document.getElementsByClassName(".copiar").style.display="initial";
-    document.getElementById("ningunTexto").style.display ="none";
-    document.getElementById("mensajeIngresarTexto").style.display="none";
+    document.getElementById("ningunTexto").style.visibility ="hidden"; 
+    document.getElementById("mensajeIngresarTexto").style.visibility="hidden";
 }
 
+//funcion para recorrer la matriz o array
 function desencriptar(stringDesencriptado){
     let matrizCodigo = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]];
     stringDesencriptado = stringDesencriptado.toLowerCase();
 
     for(let i = 0;i < matrizCodigo.length; i++){
-        if(stringDesencriptado.includes(matrizCodigo[i][0])){ 
+        if(stringDesencriptado.includes(matrizCodigo[i][0])){ // si stringDesencritado incluye en la posicion 0 hace lo siguiente
             stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);//remplaza las palabras colocadas en el punto 0 de la array(matrizCodigo) por las del punto 1 de la array[matrizCodigo] y las cambia.
         }//verifica si en matrizCodigo en el punto 0 de cada array coincice con la palabra colocada stringEncriptado
     }
     return  stringDesencriptado;
 }
+
+// Funcion  boton desencriptar
 function btnDesencriptar(){
     const textoDesencriptado = desencriptar(inputTexto.value)
     mensaje.value = textoDesencriptado;
     inputTexto.value = "";
-    document.getElementsByClassName(".copiar").style.display="initial";
-    document.getElementById("ningunTexto").style.display ="none";
-    document.getElementById("mensajeIngresarTexto").style.display="none";
+    document.getElementById("ningunTexto").style.visibility ="hidden";
+    document.getElementById("mensajeIngresarTexto").style.visibility="hidden";
 }
 
+
+//Funcion para el boton copiar
 function copiar(){
     mensaje.select();
     navigator.clipboard.writeText(mensaje.value);
     mensaje.value = "";
     mensaje.style.backgroundImage = "url(../svg/Muñeco.svg)";
-    document.getElementById("ningunTexto").style.display = document.querySelector(".ningunTexto");
-    document.getElementById("mensajeIngresarTexto").style.display= document.querySelector(".mensajeIngresarTexto");
+    document.getElementById("ningunTexto").style.display = "inherit";
+    document.getElementById("mensajeIngresarTexto").style.display= "inherit";
+    document.getElementById("ningunTexto").style.visibility ="visible";
+    document.getElementById("mensajeIngresarTexto").style.visibility="visible";
+    inputTexto.focus();
 }
